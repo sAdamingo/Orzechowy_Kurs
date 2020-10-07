23a89e9f730c8d6905332c75a7319c136912e296
Adam <ad.stelmach@gmail.com>
2020-10-07 09:09:40 +0200

    Zadanie domowe

---
+++ src/main/java/com/sAdamingo/course/lesson_1/homework/zadanie_3.java
@@ -0,0 +1,7 @@
+package com.sAdamingo.course.lesson_1.homework;
+
+public class zadanie_3 {
+    public static void main(String[] args) {
+        System.out.println("123456".length() == "234567".length());
+    }
+}
