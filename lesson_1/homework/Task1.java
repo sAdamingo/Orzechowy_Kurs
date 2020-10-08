b05ab40fc461d60d19a180bd2940242522ba18ef
sAdamingo <ad.stelmach@gmail.com>
2020-10-08 08:43:46 +0200

    Update and rename zadanie_1.java to Task1.java

--- lesson_1/homework/Task1.java
+++ lesson_1/homework/Task1.java
@@ -1,6 +1,6 @@
 package com.sAdamingo.course.lesson_1.homework;
 
-public class zadanie_1 {
+public class Task1 {
     public static void main(String[] args) {
         String inputString = "Laborki z metrologii myk i pykk";
         if (inputString.length() % 2 == 0) {
