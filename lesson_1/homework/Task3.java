a7ee4c09b962c3baa16153e36bcd6229dedc73e3
sAdamingo <ad.stelmach@gmail.com>
2020-10-08 08:45:03 +0200

    Update and rename zadanie_3.java to Task3.java

--- lesson_1/homework/Task3.java
+++ lesson_1/homework/Task3.java
@@ -1,6 +1,6 @@
 package com.sAdamingo.course.lesson_1.homework;
 
-public class zadanie_3 {
+public class Task3 {
     public static void main(String[] args) {
         System.out.println("123456".length() == "234567".length());
     }
