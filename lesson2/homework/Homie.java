81e7cd85f10057a5042788ad9eafbde5de263397
sAdamingo <ad.stelmach@gmail.com>
2020-10-12 15:33:18 +0200

    Update Homie.java

--- lesson2/homework/Homie.java
+++ lesson2/homework/Homie.java
@@ -4,13 +4,14 @@ public class Homie {
     public static void main(String[] args) {
         for (int i = 1; i <= 150; i++) {
             if (i % 3 != 0) {
-                System.out.print("\n" + i + " ");
+                System.out.print(i + " ");
                 if (i % 4 == 0) {
                     System.out.print("Zio");
                 }
                 if (i % 5 == 0) {
                     System.out.print("mek");
                 }
+                System.out.println();
             }
         }
     }
