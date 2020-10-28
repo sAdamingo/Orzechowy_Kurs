0b6ecd772724936d7c84539a8fa81d90fd0589ae
Adam <ad.stelmach@gmail.com>
2020-10-28 19:22:47 +0100

    Suggested changes applied.

--- src/main/java/com/sAdamingo/course/lesson7/GarminDataGetter.java
+++ src/main/java/com/sAdamingo/course/lesson7/GarminDataGetter.java
@@ -3,9 +3,6 @@ package com.sAdamingo.course.lesson7;
 import cz.kacirekj.connector.DataConnector;
 import cz.kacirekj.connector.LoginConnector;
 import cz.kacirekj.domain.GarminSession;
-import cz.kacirekj.exception.HttpErrorGarminException;
-import cz.kacirekj.exception.LoginGarminException;
-import cz.kacirekj.exception.TooManyRequestsGarminException;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -13,37 +10,21 @@ import java.util.Map;
 
 public class GarminDataGetter {
 
-
-    public static void main(String[] args) throws IOException {
-        // Login for GarminSession
-
+    public static void main(String[] args) {
         LoginConnector loginConnector = new LoginConnector();
         GarminSession garminSession = null;
         try {
-            garminSession = loginConnector.login("ad.stelmach@gmail.com", "asdf");
-        } catch (LoginGarminException e) {
-            e.printStackTrace();
-            return;
-        } catch (TooManyRequestsGarminException e) {
-            e.printStackTrace();
-            return;
+            garminSession = loginConnector.login("ad.stelmach@gmail.com", "13Stan12");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        DataConnector dataConnector = new DataConnector(garminSession);
+        Map<String, Object> heartRates = null;
+        try {
+            heartRates = dataConnector.getHeartRates(LocalDate.now().minusDays(1));
         } catch (IOException e) {
             e.printStackTrace();
-            return;
-        } catch (HttpErrorGarminException e) {
-            e.printStackTrace();
-            return;
         }
-
-// Fetch data from API
-
-        DataConnector dataConnector = new DataConnector(garminSession);
-        Map<String, Object> userSummary = dataConnector.getUserSummary(LocalDate.now().minusDays(1));
-        Map<String, Object> heartRates = dataConnector.getHeartRates(LocalDate.now().minusDays(1));
-
-        //System.out.println(userSummary);
         System.out.println(heartRates);
-        //System.out.println(garminSession);
-
     }
 }
