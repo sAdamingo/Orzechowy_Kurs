60ee382c549ae9203f9130f0528176ee7de7c59f
Adam <ad.stelmach@gmail.com>
2020-10-29 12:20:24 +0100

    Suggested changes applied

--- src/main/java/com/sAdamingo/course/lesson7/GarminDataGetter.java
+++ src/main/java/com/sAdamingo/course/lesson7/GarminDataGetter.java
@@ -7,19 +7,30 @@ import cz.kacirekj.domain.GarminSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GarminDataGetter {
 
     public static void main(String[] args) {
+
+        Scanner skn = new Scanner(System.in);
+        System.out.println("Please enter your e-mail: ");
+        String email = skn.nextLine();
+        System.out.println("Enter password: ");
+        String password = skn.nextLine();
         LoginConnector loginConnector = new LoginConnector();
         GarminSession garminSession = null;
+
+
         try {
-            garminSession = loginConnector.login("ad.stelmach@gmail.com", "asdf");
+            garminSession = loginConnector.login(email, password);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
         DataConnector dataConnector = new DataConnector(garminSession);
         Map<String, Object> heartRates = null;
+
         try {
             heartRates = dataConnector.getHeartRates(LocalDate.now().minusDays(1));
         } catch (IOException e) {
