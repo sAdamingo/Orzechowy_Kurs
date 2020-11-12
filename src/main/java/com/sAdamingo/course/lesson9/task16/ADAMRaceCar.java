69875553d29a7ed5c8ee46de07d8e557b8fc3113
Adam <ad.stelmach@gmail.com>
2020-11-12 11:01:16 +0100

    Implementing Task 15. Testing class AutoDealership and applying changes to task 12. Learning using Mockito repository.
    I left the dupadupa directory for educational reasons.

--- src/main/java/com/sAdamingo/course/lesson9/task16/ADAMRaceCar.java
+++ src/main/java/com/sAdamingo/course/lesson9/task16/ADAMRaceCar.java
@@ -4,31 +4,23 @@ import com.sAdamingo.course.lesson9.task16.brakes.MediumBrakes;
 import com.sAdamingo.course.lesson9.task16.brakes.StrongBrakes;
 import com.sAdamingo.course.lesson9.task16.brakes.WeakBrakes;
 import com.sAdamingo.course.lesson9.task16.engine.HugeEngine;
-import com.sAdamingo.course.lesson9.task16.engine.NormalEngine;
-import com.sAdamingo.course.lesson9.task16.transmission.NormalTransmission;
 import com.sAdamingo.course.lesson9.task16.transmission.ShortTransmission;
+import com.sAdamingo.course.lesson9.task16.tyres.LongLastingTyres;
 import com.sAdamingo.course.lesson9.task16.tyres.NormalTyres;
 import com.sAdamingo.course.lesson9.task16.tyres.SportTyres;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class ADAMRaceCar extends RaceCar {
+
     @Override
     protected void prepareForRace(List<Integer> track) {
-        if (track.size() < 6) {
-            tyres = new SportTyres();
-        } else {
+        if (track.size() > 20) {
+            tyres = new LongLastingTyres();
+        } else if (track.size() > 10) {
             tyres = new NormalTyres();
-        }
-        int maxStraight = track.stream()
-                .max(Comparator.comparingInt(Integer::intValue)).get();
-        if (maxStraight < 500) {
-            engine = new NormalEngine();
-            transmission = new NormalTransmission();
         } else {
-            engine = new HugeEngine();
-            transmission = new ShortTransmission();
+            tyres = new SportTyres();
         }
         if (track.size() <= 10) {
             brakes = new StrongBrakes();
@@ -37,6 +29,8 @@ public class ADAMRaceCar extends RaceCar {
         } else {
             brakes = new WeakBrakes();
         }
+        engine = new HugeEngine();
+        transmission = new ShortTransmission();
     }
 
     @Override
