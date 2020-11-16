3223e241872748548f9e7098bc4a844ce53208e3
Adam <ad.stelmach@gmail.com>
2020-11-16 14:39:36 +0100

    Implementing Task18

---
+++ src/main/java/com/sAdamingo/course/Task18/DataBaseOperator.java
@@ -0,0 +1,7 @@
+package com.sAdamingo.course.Task18;
+
+public class DataBaseOperator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DataBase Operator!");
+    }
+}
